cb91770f73680b776a7944ea4c1a3cbe4a95cb20
spectra.py <global-tushar.kumar@infostride.com>
2023-02-07 01:46:04 +0530

    Banner , contact and skill section completed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
+import { Skills } from './components/Skills';
 import { Banner } from './components/Banner';
+import {Projects} from './components/Projects';
+import { ProjectCard } from './components/ProjectsCard';
+
 
 function App() {
   return (
     <div className="App">
 
-      <NavBar />  
+      <NavBar />
+
       <Banner />
 
+      <Skills/>
+
+       <Projects/>
+       
+       <ProjectCard/>
     </div>
   );
 }
